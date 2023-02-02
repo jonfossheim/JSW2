5b96668909b456de2c138dd6e9159aad16dd2bc7
fossheim <fossheimjoneven@gmail.com>
2023-02-02 08:29:10 +0100

    removal

--- js/asideCode.js
+++ js/asideCode.js
@@ -29,3 +29,20 @@ listItems.forEach((li) => {
     li.classList.toggle('done');
   };
 });
+
+todoInput.maxLength = 18;
+message.innerHTML = 0;
+
+todoInput.onkeyup = () => {
+  let inLen = todoInput.value.length;
+  message.innerHTML = inLen;
+  if (inLen === 18) {
+    message.style.color = 'red';
+    setTimeout(() => {
+      message.style.cssText = 'display: none';
+    }, 1000);
+  } else {
+    message.style.color = 'black';
+    message.style.cssText = 'display: inline-block';
+  }
+};
--- js/script.js
+++ js/script.js
@@ -3,19 +3,22 @@ const addTodo = document.querySelector('.add-todo');
 const todoList = document.querySelector('.todo-list');
 const message = document.querySelector('.message');
 
-todoInput.maxLength = 18;
-message.innerHTML = 0;
-
-todoInput.onkeyup = () => {
-  let inLen = todoInput.value.length;
-  message.innerHTML = inLen;
-  if (inLen === 18) {
-    message.style.color = 'red';
+addTodo.onclick = () => {
+  const value = todoInput.value.trim();
+  if (value === '') {
+    const li = document.createElement('li');
+    li.innerHTML = 'Please provide a string';
+    todoList.appendChild(li);
     setTimeout(() => {
-      message.style.cssText = 'display: none';
-    }, 1000);
+      li.remove();
+    }, 2000);
   } else {
-    message.style.color = 'black';
-    message.style.cssText = 'display: inline-block';
+    const li = document.createElement('li');
+    li.innerHTML = `${value} <button>Toggle</button>`;
+    li.onclick = (event) => {
+      event.currentTarget.style.color = 'red';
+      li.classList.toggle('done');
+    };
+    todoList.appendChild(li);
   }
 };
